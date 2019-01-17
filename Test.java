a7857091442a98ca8580492559baeeb294d58916
k391160334 <maseqqqq@gmail.com>
2019-01-17 12:34:17 +0900

    init

--- Test.java
+++ Test.java
@@ -10,12 +10,11 @@ public class Test {
 		boolean which;
 		Tiger tiger=new Tiger();
 		Rabbit rabbit=new Rabbit();
-		System.out.println("영세한 중앙동물원에는 토끼와 호랑이가 있습니다. 토끼에 대한 정보가 궁금하시다면 false, 호랑이에 대한 정보가 궁금하시다면 true을 눌러주세요.");
+		System.out.println("영세한 중앙동물원에는 토끼와 호랑이가 있습니다. 토끼에 대한 정보가 궁금하시다면 false, 호랑이에 대한 정보가 궁금하시다면 true를 눌러주세요.");
 		Scanner scan=new Scanner(System.in);
 		animal=scan.nextBoolean();
 		if(animal) {
-			
-			System.out.println("호랑이의 먹이가 궁금하다면 false, 씻기가 궁금하다면 true을 눌러주세요.");
+			System.out.println("호랑이의 먹이가 궁금하다면 false, 씻기가 궁금하다면 true를 눌러주세요.");
 			which=scan.nextBoolean();
 			if(which)
 				tiger.clean();
@@ -23,8 +22,7 @@ public class Test {
 				tiger.prey();
 		}
 		else {
-			
-			System.out.println("토끼의 먹이가 궁금하다면 0, 씻기가 궁금하다면 1을 눌러주세요.");
+			System.out.println("토끼의 먹이가 궁금하다면 false, 씻기가 궁금하다면 true를 눌러주세요.");
 			which=scan.nextBoolean();
 			if(which)
 				rabbit.clean();
